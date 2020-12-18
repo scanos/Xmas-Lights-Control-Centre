d1e11dfbae6db84698737bc04a7695cb5eff4e3c
root <root@scanos.hopto.org>
2020-12-18 11:15:02 +0000

    some redundant comments removed

--- xmas_lights_toggle.sh
+++ xmas_lights_toggle.sh
@@ -26,7 +26,6 @@ lights_array=($lights_array)
            }
 
 
-#lights_array=(kitchen_ceiling_lights 192.168.1.237 bedroom_lights 192.168.1.241 hall_lights 192.168.1.242 hall_ceiling_lights 192.168.1.65 kitchen_lights 192.168.1.233)
 
 
 
